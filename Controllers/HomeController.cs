19246903fbd3db456809ee1417cb9f8fe7bbea1c
Suvash Shah Thakuri <suvash@appmozart.com>
2021-06-19 16:02:22 -0700

    Contact Us page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,12 @@ namespace shangrila.Controllers
             return View();
         }
 
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
